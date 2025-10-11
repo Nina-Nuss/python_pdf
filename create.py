616f63bc970e426427a1d71e9345823770837425
Your Name <your.email@example.com>
2025-10-11 22:03:43 +0200

    .

--- create.py
+++ create.py
@@ -22,7 +22,8 @@ c.setFont("Helvetica", 9)
 c.drawString(120, 740, "1: Installation Komandozeile: pip install reportlab")
 c.drawString(120, 720, "2: from reportlab.pdfgen import canvas from reportlab.lib.pagesizes import A4")
 
-c.drawString(120, 700, "3: c = canvas.Canvas('dateiname.pdf', pagesize=A4) erstellt ein neues PDF-Dokument")
+c.drawString(120, 700, "3: c = canvas.Canvas('dateiname.pdf', pagesize=A4)")
+c.drawString(350, 700, "# erstellt ein neues PDF-Dokument")
 
 
 # Untertitel
@@ -32,18 +33,28 @@ c.drawString(100, 680, "Wichtige Funktionen von ReportLab:")
 # Informationen in logischer Reihenfolge
 c.setFont("Helvetica", 9)
 
-c.drawString(120, 660, "-  c.setFont(schriftart, größe) setzt die Schriftart und Größe, z.B. ('Helvetica', 12)")
-c.drawString(120, 640, "-  c.drawString(x, y, 'text') fügt Text an der Position x, y hinzu, z.B. (100, 700, 'Hallo Welt')")
+c.drawString(120, 660, "-  c.setFont(schriftart, größe) ")
+c.drawString(280, 660, "# setzt die Schriftart und Größe, z.B. ('Helvetica', 12)")
+
+c.drawString(120, 640, "-  c.drawString(x, y, 'text')")
+c.drawString(280, 640, "# fügt Text an der Position x, y hinzu, z.B. (100, 700, 'Hallo Welt')")
 
 # Farbiges Beispiel
 c.setFillColorRGB(0, 0, 1)  # Farbe auf Blau setzen (RGB-Werte von 0 bis 1)
-c.drawString(120, 620, "-  c.setFillColorRGB(rot, grün, blau) setzt die Füllfarbe für den Text, z.B. (0, 0, 1) für Blau")
+c.drawString(120, 620, "-  c.setFillColorRGB(rot, grün, blau) ")
+c.drawString(280, 620, "# setzt die Füllfarbe für den Text, z.B. (0, 0, 1) für Blau")
 
 # Farbe zurücksetzen
 c.setFillColorRGB(0, 0, 0)  # Farbe auf Schwarz zurücksetzen
-c.drawString(120, 600, "-  c.line(x1, y1, x2, y2) zeichnet eine Linie zwischen zwei Punkten, z.B. (100, 600, 400, 600)")
+c.drawString(120, 600, "-  c.line(x1, y1, x2, y2)")
+c.drawString(280, 600, "# zeichnet eine Linie zwischen zwei Punkten, z.B. (100, 600, 400, 600)")
 
-c.drawString(100, 560, "URL: https://docs.reportlab.com/")
+c.drawString(100, 570, "reminder:    x: Links, Rechts (Horizontal-Achse)     y: Unten, Oben (Vertikal-Achse)")
+c.setFont("Helvetica", 10)
+text = "(๑´• .̫ •ू`๑)"
+c.drawString(100, 500, text)
+
+c.drawString(100, 540, "URL: https://docs.reportlab.com/")
 
 # PDF speichern
 c.save()
